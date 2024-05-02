470f8af68f1e11735d5b327c4914ca0c85df9b96
kewensilva <kewen.macedo@gmail.com>
2024-05-02 05:56:45 -0300

    feat: created server and routes with typescript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log(`Olá mundo!`);
+import express from "express";
+import "dotenv/config";
+import routes from "./routes";
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+app.listen(`${process.env.PORT}`, ()=>{
+  console.log(`servidor Online no endereço: http://localhost:${process.env.PORT}`); 
+});
